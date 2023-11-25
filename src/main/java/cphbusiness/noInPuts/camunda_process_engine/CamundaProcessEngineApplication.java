aeda457c363f73bb06c8dbe0e151b22636cecadb
Benjamin S <39574422+pl0xi@users.noreply.github.com>
2023-11-26 00:57:53 +0100

    Camunda Service Init

---
+++ src/main/java/cphbusiness/noInPuts/camunda_process_engine/CamundaProcessEngineApplication.java
@@ -0,0 +1,13 @@
+package cphbusiness.noInPuts.camunda_process_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaProcessEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaProcessEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cphbusiness/noInPuts/camunda_process_engine/CamundaProcessEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package cphbusiness.noInPuts.camunda_process_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaProcessEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
