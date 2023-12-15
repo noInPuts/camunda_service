44d4c5a0e97faeadb5d17e4f3f771adbc8b79187
Benjamin S <39574422+pl0xi@users.noreply.github.com>
2023-12-15 18:07:00 +0100

    pmd violation fix

--- src/main/java/cphbusiness/noInPuts/camunda_process_engine/facades/ServiceFacade.java
+++ src/main/java/cphbusiness/noInPuts/camunda_process_engine/facades/ServiceFacade.java
@@ -3,8 +3,6 @@ package cphbusiness.noInPuts.camunda_process_engine.facades;
 import camunda_process_engine.noinputs.cphbusiness.soap.StartRestaurantApplicationRequest;
 import camunda_process_engine.noinputs.cphbusiness.soap.StartRestaurantApplicationResponse;
 
-import java.util.Map;
-
 public interface ServiceFacade {
     StartRestaurantApplicationResponse startRestaurantApplicationProcess(StartRestaurantApplicationRequest restaurantForm);
 }
--- src/main/java/cphbusiness/noInPuts/camunda_process_engine/soap/CamundaApplicationEndpoint.java
+++ src/main/java/cphbusiness/noInPuts/camunda_process_engine/soap/CamundaApplicationEndpoint.java
@@ -3,16 +3,12 @@ package cphbusiness.noInPuts.camunda_process_engine.soap;
 import camunda_process_engine.noinputs.cphbusiness.soap.StartRestaurantApplicationRequest;
 import camunda_process_engine.noinputs.cphbusiness.soap.StartRestaurantApplicationResponse;
 import cphbusiness.noInPuts.camunda_process_engine.facades.ServiceFacade;
-import cphbusiness.noInPuts.camunda_process_engine.service.ProcessService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Endpoint
 public class CamundaApplicationEndpoint {
